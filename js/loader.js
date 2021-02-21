1b481c9d5e1998bb0871e95419249e36c31a29f7
ralsabbagh <r.alsabbagh@tap.company>
2021-02-22 01:22:37 +0300

    clean loader code

--- js/loader.js
+++ js/loader.js
@@ -1,38 +1,38 @@
-var loaderSvg = getElmntByClass('box-loader');
-loaderSvg.setAttribute('style', 'transform: scale(1.1);');
+var boxLoader = getElmntByClass('box-loader');
+boxLoader.setAttribute('style', 'transform: scale(1.1);');
 
-var circle1 = getElmntByClass('path-loader');
-circle1.setAttribute('cx', '50%');
-circle1.setAttribute('cy', '50%');
-circle1.setAttribute('r', '20');
-circle1.setAttribute('fill', 'none');
-circle1.setAttribute(
+var pathLoader = getElmntByClass('path-loader');
+pathLoader.setAttribute('cx', '50%');
+pathLoader.setAttribute('cy', '50%');
+pathLoader.setAttribute('r', '20');
+pathLoader.setAttribute('fill', 'none');
+pathLoader.setAttribute(
   'style',
   'stroke: #797574; animation: 3s linear 0s infinite normal none running ring-outer'
 );
 
-var animateTransform1 = getElmntByClass('transform-loader');
-animateTransform1.setAttribute('attributeName', 'transform');
-animateTransform1.setAttribute('type', 'rotate');
-animateTransform1.setAttribute('from', '0 50 50');
-animateTransform1.setAttribute('to', '360 50 50');
-animateTransform1.setAttribute('dur', '1s');
-animateTransform1.setAttribute('repeatCount', 'indefinite');
+var transformLoader = getElmntByClass('transform-loader');
+transformLoader.setAttribute('attributeName', 'transform');
+transformLoader.setAttribute('type', 'rotate');
+transformLoader.setAttribute('from', '0 50 50');
+transformLoader.setAttribute('to', '360 50 50');
+transformLoader.setAttribute('dur', '1s');
+transformLoader.setAttribute('repeatCount', 'indefinite');
 
-var circle2 = getElmntByClass('path-loader-inner');
-circle2.setAttribute('cx', '50');
-circle2.setAttribute('cy', '50');
-circle2.setAttribute('r', '14');
-circle2.setAttribute('fill', 'none');
-circle2.setAttribute(
+var pathLoaderInner = getElmntByClass('path-loader-inner');
+pathLoaderInner.setAttribute('cx', '50');
+pathLoaderInner.setAttribute('cy', '50');
+pathLoaderInner.setAttribute('r', '14');
+pathLoaderInner.setAttribute('fill', 'none');
+pathLoaderInner.setAttribute(
   'style',
   'stroke: #797574; animation: 3s linear 0s infinite normal none running ring-inner'
 );
 
-var animateTransform2 = getElmntByClass('transform-loader-inner');
-animateTransform2.setAttribute('attributeName', 'transform');
-animateTransform2.setAttribute('type', 'rotate');
-animateTransform2.setAttribute('from', '360 50 50');
-animateTransform2.setAttribute('to', '0 50 50');
-animateTransform2.setAttribute('dur', '1s');
-animateTransform2.setAttribute('repeatCount', 'indefinite');
+var transformLoaderInner = getElmntByClass('transform-loader-inner');
+transformLoaderInner.setAttribute('attributeName', 'transform');
+transformLoaderInner.setAttribute('type', 'rotate');
+transformLoaderInner.setAttribute('from', '360 50 50');
+transformLoaderInner.setAttribute('to', '0 50 50');
+transformLoaderInner.setAttribute('dur', '1s');
+transformLoaderInner.setAttribute('repeatCount', 'indefinite');
