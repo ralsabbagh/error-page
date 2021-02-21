5477e078eb9428e9a06845cd4e52a54720c0186e
ralsabbagh <r.alsabbagh@tap.company>
2021-02-21 18:13:54 +0300

    clean addClass function

--- js/addClass.js
+++ js/addClass.js
@@ -1,7 +1,5 @@
 function addClass(element, className) {
-  var isAR, element, arr;
-  isAR = window.location.href.indexOf('lang=ar') > -1;
-  arr = element.className.split(' ');
+  var arr = element.className.split(' ');
   if (arr.indexOf(className) == -1) {
     element.className += ' ' + className;
   }
